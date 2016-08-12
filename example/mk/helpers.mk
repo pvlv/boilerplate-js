6fbfe21c9ec7cd0828daea4da86e33ab5d1742f8
Ivan Pavlov <ivanpavlovv@gmail.com>
2016-08-12 15:49:54 +0300

    fix prerequisite in target "in" -> Makefile

--- example/mk/helpers.mk
+++ example/mk/helpers.mk
@@ -4,4 +4,4 @@ in: PROJECT_DIR := $(CURDIR)/..
 in: DCWORK_DIR := $(WORK_DIR)/$(EXAMPLE_DIR)
 in: DCPORT := $(LOCAL_PORT):$(CONTAINER_PORT)
 in: DCCMD := bash
-in: $(NPM) docker_container
+in: docker_container
